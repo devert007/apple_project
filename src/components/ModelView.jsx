98a29b7590d191312c58a887670f27c57c08f39c
devert007 <kislydevert007@gmail.com>
2024-11-12 17:56:37 +0300

    fixes

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -8,7 +8,7 @@ import {
 import * as THREE from "three";
 import Lights from "./Lights";
 import Loader from "./Loader";
-import IPhone from "../components/IPhone";
+import IPhone from "./Iphone";
 import { Suspense } from "react";
 
 const ModelView = ({
